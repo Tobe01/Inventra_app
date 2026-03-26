4c55ad48e51bf0e03718fa14cfca1cb8e0023cda
Tobechi Duru, B.Eng <tobechi.duru001@gmail.com>
2026-03-26 16:47:29 -0700

    updates

--- stockflow/src/components/sidenav.jsx
+++ stockflow/src/components/sidenav.jsx
@@ -5,17 +5,20 @@ import documents from '../assets/icons/proicons--document.svg';
 
 export function Sidenav(){
   return(
-    <div className='bg-text fixed left-0 top-0 bottom-0 flex flex-col align-middle justify-between py-5 px-2'>
-      <div className='flex flex-col align-middle justify-center space-y-3.5 pb-5 border-b border-border'>
-        <span className='text-white bg-secondary font-bold text-20 rounded-md text-center justify-items-center py-2.5 cursor-pointer'>SF</span>
-        <span className='bg-neutral-700 rounded-md text-center justify-items-center p-2.5 cursor-pointer'><img src={grid} alt='pos-icon' loading='lazy' /></span>
-        <span className='bg-neutral-700 rounded-md text-center justify-items-center p-2.5 cursor-pointer'><img src={cube} alt='inventory-icon' loading='lazy' /></span>
-        <span className='bg-neutral-700 rounded-md text-center justify-items-center p-2.5 cursor-pointer'><img src={documents} alt='orders-icon' loading='lazy' /></span>
+    <div className='bg-text fixed left-0 top-0 bottom-0 flex flex-col align-middle justify-between py-5 px-4'>
+      <div className='flex flex-col align-middle justify-center space-y-5'>
+        <button className='text-white bg-secondary font-bold text-20 rounded-md text-center justify-items-center w-9 ml-auto mr-auto py-1 cursor-pointer'>SF</button>
+          <div className='flex flex-col space-y-1'>
+            <button className='bg-neutral-700 rounded-md text-center justify-items-center py-2 px-2.5 cursor-pointer'><img src={grid} alt='pos-icon' loading='lazy' /></button>
+            <button className='bg-neutral-700 rounded-md text-center justify-items-center py-2 px-2.5 cursor-pointer'><img src={cube} alt='inventory-icon' loading='lazy' /></button>
+            <button className='bg-neutral-700 rounded-md text-center justify-items-center py-2 px-2.5 cursor-pointer'><img src={documents} alt='orders-icon' loading='lazy' /></button>
+          </div>
+        <div className='border-b border-border w-7 m-auto'></div>
       </div>
 
-      <div className='flex flex-col align-middle justify-center space-y-3.5'>
-        <span className='text-center justify-items-center cursor-pointer'><img src={settings} alt='settings-icon' loading='lazy' /></span>
-        <span className='text-white font-bold text-20 bg-neutral-700 rounded-md text-center justify-items-center p-2.5 cursor-pointer'>AO</span>
+      <div className='flex flex-col align-middle justify-center space-y-1'>
+        <button className='text-center justify-items-center  bg-neutral-700 rounded-md py-2 px-2.5 cursor-pointer'><img src={settings} alt='settings-icon' loading='lazy' /></button>
+        <button className='text-white font-bold bg-neutral-700 rounded-md text-center justify-items-center py-2 px-2.5 cursor-pointer'>AO</button>
       </div>
     </div>
   )
