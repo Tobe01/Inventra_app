8453f3a63258dc49706ad2bb074828f9c0136083
Tobechi Duru, B.Eng <tobechi.duru001@gmail.com>
2026-04-06 14:50:31 -0700

    updates!

--- stockflow/src/pages/inventory.jsx
+++ stockflow/src/pages/inventory.jsx
@@ -42,7 +42,7 @@ export function InventoryPage(){
 
         <div className='rounded-bl-xl rounded-br-xl'>
 
-          <div className='flex align-middle py-1.5 bg-input rounded-tl-lg rounded-tr-lg border border-border gap-2'>
+          <div className='flex align-middle text-light py-1.5 bg-input rounded-tl-lg rounded-tr-lg border border-border gap-2'>
               <div className='w-90 my-auto pl-3.5'>Ingredient</div>
               <div className='w-65 my-auto pl-3.5'>In Stock</div>
               <div className='w-65 my-auto pl-3.5'>Min. Level</div>
@@ -52,85 +52,85 @@ export function InventoryPage(){
           
           <div className='rounded-bl-lg rounded-br-lg border-x border-border'>
 
-            <div className='flex border-b rounded-bl-lg rounded-br-lg border-border align-middle p-2 justify-items-start gap-2'>
+            <div className='flex border-b rounded-bl-lg rounded-br-lg border-border align-middle py-3 hover:bg-input justify-items-start gap-2'>
 
               <div className='flex w-[351.5px] pl-3.5 align-middle gap-2'>
                 <div className='my-auto flex gap-2'>
                   <div className='flex gap-2'>
                     <img src={cookies} width={30} alt='itemImage' loading='lazy' />
-                    <p className='m-auto'>Parboiled Rice</p>
+                    <p className='m-auto font-bold'>Parboiled Rice</p>
                   </div>
-                  <button className='bg-secondary text-white p-1 rounded-sm m-auto text-[10px]'>OK</button>
+                  <button className='bg-lightgreen px-2 py-0.5 rounded-full text-secondary font-bold m-auto text-[10px]'>OK</button>
                 </div>
               </div>
 
-              <div className='w-67 pl-3.5 my-auto'><p>85 kg</p></div>
+              <div className='w-67 pl-3.5 my-auto font-bold font-money'><p>85 kg</p></div>
 
-              <div className='w-60 my-auto'><p>20 kg</p></div>
+              <div className='w-60 my-auto text-light font-money'><p>50 kg</p></div>
 
               <div className='w-90 pl-4 flex align-middle justify-between pr-8'>
-                <div className='flex my-auto gap-2'>
-                  <p>85%</p>
+                <div className='flex my-auto gap-2 text-light'>
+                  <p className='font-money text-[10px]'>85%</p>
                   <progress className='my-auto bg-amber-500' value="70" max="100"></progress>
                 </div>
-                <div className='bg-blue-600'>
-                  <button className='bg-purple-500 p-1 rounded-sm m-auto'>+ Restock</button>
+                <div className='m-auto'>
+                  <button className='px-1 rounded-sm m-auto text-light border border-border'>+ Restock</button>
                 </div>
               </div>
 
             </div>
 
-            <div className='flex border-b bg-lightred rounded-bl-lg rounded-br-lg border-border align-middle p-2 justify-items-start gap-2'>
+            <div className='flex border-b bg-critical rounded-bl-lg rounded-br-lg border-border align-middle py-3 hover:bg-input justify-items-start gap-2'>
 
               <div className='flex w-[351.5px] pl-3.5 align-middle gap-2'>
                 <div className='my-auto flex gap-2'>
                   <div className='flex gap-2'>
                     <img src={cookies} width={30} alt='itemImage' loading='lazy' />
-                    <p className='m-auto'>Parboiled Rice</p>
+                    <p className='m-auto font-bold'>Parboiled Rice</p>
                   </div>
-                  <button className='bg-secondary text-white p-1 rounded-sm m-auto text-[10px]'>Critical</button>
+                  <button className='bg-lightred text-secondaryred px-2 py-0.5 rounded-full font-bold m-auto text-[10px]'>Critical</button>
                 </div>
               </div>
 
-              <div className='w-67 pl-3.5 my-auto'><p>85 kg</p></div>
+              <div className='w-67 pl-3.5 my-auto font-bold font-money'><p>85 kg</p></div>
 
-              <div className='w-60 my-auto'><p>20 kg</p></div>
+              <div className='w-60 my-auto text-light font-money'><p>40 L</p></div>
 
               <div className='w-90 pl-4 flex align-middle justify-between pr-8'>
-                <div className='flex my-auto gap-2'>
-                  <p>85%</p>
+                <div className='flex my-auto gap-2 text-light'>
+                  <p className='font-money text-[10px]'>85%</p>
                   <progress className='my-auto bg-amber-500' value="70" max="100"></progress>
                 </div>
-                <div className='bg-blue-600'>
-                  <button className='bg-purple-500 p-1 rounded-sm m-auto'>+ Restock</button>
+                <div className='m-auto'>
+                  <button className='px-1 rounded-sm m-auto text-light border border-border'>+ Restock</button>
                 </div>
               </div>
 
             </div>
 
-            <div className='flex border-b bg-pendingfade rounded-bl-lg rounded-br-lg border-border align-middle p-2 justify-items-start gap-2'>
+            <div className='flex border-b bg-low rounded-bl-lg rounded-br-lg border-border align-middle py-3 hover:bg-input justify-items-start gap-2'>
 
               <div className='flex w-[351.5px] pl-3.5 align-middle gap-2'>
                 <div className='my-auto flex gap-2'>
                   <div className='flex gap-2'>
                     <img src={cookies} width={30} alt='itemImage' loading='lazy' />
-                    <p className='m-auto'>Parboiled Rice</p>
+                    <p className='m-auto font-bold'>Parboiled Rice</p>
                   </div>
-                  <button className='bg-secondary text-white p-1 rounded-sm m-auto text-[10px]'>Low</button>
+                  <button className='bg-pendingfade text-pending px-2 font-bold py-0.5 rounded-full m-auto text-[10px]'>Low</button>
                 </div>
               </div>
 
-              <div className='w-67 pl-3.5 my-auto'><p>85 kg</p></div>
+              <div className='w-67 pl-3.5 my-auto font-bold font-money'><p>85 kg</p></div>
 
-              <div className='w-60 my-auto'><p>20 kg</p></div>
+              <div className='w-60 my-auto text-light font-money'><p>20 kg</p></div>
 
               <div className='w-90 pl-4 flex align-middle justify-between pr-8'>
-                <div className='flex my-auto gap-2'>
-                  <p>85%</p>
+                <div className='flex my-auto gap-2 text-light'>
+                  <p className='font-money text-[10px]'>85%</p>
                   <progress className='my-auto bg-amber-500' value="70" max="100"></progress>
                 </div>
-                <div className='bg-blue-600'>
-                  <button className='bg-purple-500 p-1 rounded-sm m-auto'>+ Restock</button>
+                <div className='m-auto'>
+                  <button className='px-1 rounded-sm m-auto text-light border border-border'>+ Restock</button>
                 </div>
               </div>
 
